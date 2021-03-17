543a999af01180e8e3b9e30043ab84257f50fb5d
mintmtt <satidasomjai@gmail.com>
2021-03-18 03:27:23 +0700

    l1

---
+++ pages/inspire.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">
+          Login successful
+        </v-list-item-title>
+      </v-list-item-content>
+
+
+    </v-list-item>
+  </v-card>
+</template>
